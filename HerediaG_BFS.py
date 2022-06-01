62788db34826df9729ca031c5ec925ad21c27ab8
gbheredia22Z <gbheredia@espe.edu.ec>
2022-05-31 22:49:01 -0500

    Agregación de nodos a la ista de adyacencia por medio de parámetros

--- HerediaG_BFS.py
+++ HerediaG_BFS.py
@@ -22,11 +22,15 @@ class Grafo:
             Tipo de grafo: dirigido o no dirigido.
         m_lista_adyacencia : diccionario
             Diccionario que almacena el valor de los nodos mediante una lista de adyacencia
-
     """
     def __init__(self, num_de_nodos, dirigido=True):
-        '''Definición del constructor con parametros, que recibirá el numero de nodos que tendrá la clase Grafo.'''
-        
+        """
+        Definición del constructor con parametros, que recibirá el numero de nodos que tendrá la clase Grafo 
+        Este método se encarga de recibir el (M_num_de_nodos), para a partir de eso crear un rango de nodos (m_nodos) e
+        indicar el tipo de grafo con el que se está trabajando (m_dirigido) y po último se crea el diccionario de datos mediante 
+        la figuración del grafo con una lista de adyacencia
+        """
+
         #Definición de número de nodos del Grafo
         self.m_num_de_nodos = num_de_nodos
         #Especificación de rango de nodos del Grafo
@@ -37,3 +41,22 @@ class Grafo:
 
         #Figurar el grafo mediante una lista de adyacencia 
         self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}#Implementación de lista de adyacencia mediante un diccionario
+    
+    def agregar_borde(self, nodo1, nodo2, peso=1):
+        """Se agrega nodos a la lista de adyacencia recibiendo como parámetros : nodo1, nodo2 y el peso
+        Posteriormente los nodos son agregados a la lista de adyacencia, evaluando los parámetros:
+
+        nodo1: int
+        nodo2: int
+        peso: int
+
+        Retorno: nada
+        """
+        #Agregar nodo 2 a lista de adyacencia en nodo 1
+        self.m_lista_adyacencia[nodo1].add((nodo2, peso)) 
+        
+        #Si el nodo no es dirigido
+        if not self.m_dirigido:
+            # Agregar nodo 1 a lista de adyacencia en nodo2
+            self.m_lista_adyacencia[nodo2].add((nodo1, peso))
+            
