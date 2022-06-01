d10cf3048efac5a0f576807f4d77c04bfc44a73e
gbheredia22Z <gbheredia@espe.edu.ec>
2022-05-31 22:24:46 -0500

    Definición del constructor con los métodos funcionales

--- HerediaG_BFS.py
+++ HerediaG_BFS.py
@@ -2,4 +2,38 @@
 Nombre: Génesis Belén Heredia 
 Curso: Sexto A
 Fecha: 2/06/2022
+Materia: Inteligencia Artificial
 '''
+
+#Importar libreria Queue para generar colas / listas en el programa
+from queue import Queue
+
+#Se crea la clase Grafo, dentro de la cual instanciaremos un objeto y a su vez se generaran nodos.
+class Grafo:
+    """
+    Nuestra clase Grafo se encargará de representar el grafo con los atributos
+    y funcionalidades de estos. 
+    Definimos los atributos
+    m_numero_nodos : int
+            Nodos que forman parte del grafo
+        m_nodos : int
+            Especificación de rango de nodos del grafo
+        m_dirigido : boolean
+            Tipo de grafo: dirigido o no dirigido.
+        m_lista_adyacencia : diccionario
+            Diccionario que almacena el valor de los nodos mediante una lista de adyacencia
+
+    """
+    def __init__(self, num_de_nodos, dirigido=True):
+        '''Definición del constructor con parametros, que recibirá el numero de nodos que tendrá la clase Grafo.'''
+        
+        #Definición de número de nodos del Grafo
+        self.m_num_de_nodos = num_de_nodos
+        #Especificación de rango de nodos del Grafo
+        self.m_nodos = range(self.m_num_de_nodos)
+
+        #Establecer tipo de grafo (Dirigido o No Dirigido)
+        self.m_dirigido = dirigido
+
+        #Figurar el grafo mediante una lista de adyacencia 
+        self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}#Implementación de lista de adyacencia mediante un diccionario
